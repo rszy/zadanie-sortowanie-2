5928950201f15e8c2a8230ce8bf84d17a6ae2d3b
Ryszard Szymanski <ryszymanski@gmail.com>
2017-03-14 10:13:39 +0100

    Zadanie 2

--- src/main/java/pl/javastart/exercise/sorting2/Sorter.java
+++ src/main/java/pl/javastart/exercise/sorting2/Sorter.java
@@ -1,12 +1,43 @@
 package pl.javastart.exercise.sorting2;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Sorter {
 
 
     public void sort(List<Album> albums, SortBy sortBy, SortOrder sortOrder) {
-
-
+        if (sortBy == SortBy.TITLE && sortOrder == SortOrder.ASC) {
+            Collections.sort(albums, new Comparator<Album>() {
+                @Override
+                public int compare(Album o1, Album o2) {
+                    return o1.getTitle().compareTo(o2.getTitle());
+                }
+            });
+        } else if (sortBy == SortBy.TITLE && sortOrder == SortOrder.DESC) {
+            Collections.sort(albums, new Comparator<Album>() {
+                @Override
+                public int compare(Album o1, Album o2) {
+                    return o1.getTitle().compareTo(o2.getTitle())*-1;
+                }
+            });
+        }
+        if (sortBy == SortBy.RELEASE_DATE && sortOrder == SortOrder.ASC) {
+            Collections.sort(albums, new Comparator<Album>() {
+                @Override
+                public int compare(Album o1, Album o2) {
+                    return o1.getReleaseDate().compareTo(o2.getReleaseDate());
+                }
+            });
+        } else if(sortBy == SortBy.RELEASE_DATE && sortOrder == SortOrder.DESC) {
+            Collections.sort(albums, new Comparator<Album>() {
+                @Override
+                public int compare(Album o1, Album o2) {
+                    return o1.getReleaseDate().compareTo(o2.getReleaseDate())*-1;
+                }
+            });
+        }
     }
 }
+
